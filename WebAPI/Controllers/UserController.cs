e59b78206365f8190981d39ba16d7caed32f1fbf
Masud23m <masudcasanova@hotmail.com>
2023-10-02 14:28:08 +0200

    add files

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController :ControllerBase
+{
+    private readonly IUserLogic UserLogic;
+
+    public UserController(IUserLogic userLogic)
+    {
+        UserLogic = userLogic;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateAsync(UserCreationDto dto)
+    {
+        try
+        {
+            User user = await UserLogic.CreateAsync(dto);
+            return Created($"/users/{user.UserName}", user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
+
